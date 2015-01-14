cf3f0fc98ac33d5104f6d7a16158e37e533d1ad6
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2015-01-14 08:13:25 +0530

    changing manifest

--- Reminder/src/com/teamNIKaml/reminder/activity/LoginActivity.java
+++ Reminder/src/com/teamNIKaml/reminder/activity/LoginActivity.java
@@ -15,13 +15,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends Activity {
-	
+
 	private EditText password;
 	private Button login;
 	private SharedPreferences pref;
 	private Editor editor;
-	private Login  logindetails = new Login();
-	
+	private Login logindetails = new Login();
+
 	@Override
 	protected void onRestart() {
 		// TODO Auto-generated method stub
@@ -32,85 +32,70 @@ public class LoginActivity extends Activity {
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		//finish();
+		// finish();
 	}
 
-
-
-
-
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		init();
 		login.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
 				processLogin();
 			}
 		});
-		
+
 	}
-	
-	private void processLogin()
-	{
-		
-		if(logindetails.getLoginStatus() == 100)
-		{
-			if(!logindetails.getPassword().equals(password.getText().toString()))
-			{				
-				Toast.makeText(this,"Invalid PAssword",Toast.LENGTH_LONG).show();
+
+	private void processLogin() {
+
+		if (logindetails.getLoginStatus() == 100) {
+			if (!logindetails.getPassword().equals(
+					password.getText().toString())) {
+				Toast.makeText(this, "Invalid PAssword", Toast.LENGTH_LONG)
+						.show();
 			}
-		}
-		else
-		{
+		} else {
 			writeSharedPreference();
 		}
-		
-		startActivity(new Intent(this,PostLoginActivity.class));
+
+		startActivity(new Intent(this, PostLoginActivity.class));
 	}
-	
+
 	private void writeSharedPreference() {
 		// TODO Auto-generated method stub
-		
-		pref = getApplicationContext().getSharedPreferences("localdiskchildlocator", 0);
+
+		pref = getApplicationContext().getSharedPreferences(
+				"localdiskchildlocator", 0);
 		editor = pref.edit();
 		Log.e("writeToSharedPreferences", "localdiskchildlocator");
-		editor.putInt("loginstatus", 100);	
-		editor.putString("password",password.getText().toString());
-		editor.commit();	
-		
+		editor.putInt("loginstatus", 100);
+		editor.putString("password", password.getText().toString());
+		editor.commit();
+
 	}
-	
-	private void getLogininfo()
-	{
+
+	private void getLogininfo() {
 		pref = getApplicationContext().getSharedPreferences(
 				"localdiskchildlocator", 0);
 		logindetails.setPassword(pref.getString("password", ""));
 		logindetails.setLoginStatus(pref.getInt("loginstatus", 0));
 	}
-	
-	
-	
-	
-	
-	private void init()
-	{
-		
-		password = (EditText)findViewById(R.id.password);
-		login =(Button) findViewById(R.id.loginButton);
+
+	private void init() {
+
+		password = (EditText) findViewById(R.id.password);
+		login = (Button) findViewById(R.id.loginButton);
 		getLogininfo();
-		if(logindetails.getLoginStatus() != 100)
-		{
+		if (logindetails.getLoginStatus() != 100) {
 			login.setText("Register");
 			password.setHint("Enter your new password");
 		}
-		
+
 	}
 
-	
 }
