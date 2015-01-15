c3fb115f62204dbe4f2bf9783f2444480b1a257c
Nikhil Vijayakumar <nikhil.vijaykumar@gmail.com>
2015-01-15 10:34:18 +0530

    update delete with where clause 15-1-2015 10:35am

--- Reminder/src/com/teamNIKaml/reminder/fragment/EditPasswordDialog.java
+++ Reminder/src/com/teamNIKaml/reminder/fragment/EditPasswordDialog.java
@@ -19,18 +19,34 @@ import com.teamNIKaml.reminder.property.Constants;
 import com.teamNIKaml.reminder.property.PasswordDataSource;
 
 public class EditPasswordDialog extends DialogFragment {
-	
-	private PasswordDataSource dataSource = PasswordDataSource.getPasswordDataSource();
-	private EditText accountName,username,password;	
+
+	private PasswordDataSource dataSource = PasswordDataSource
+			.getPasswordDataSource();
+	private EditText accountName, username, password;
 	private Spinner catagory;
 	private IDBHelper dbHelper = new PasswordHelper();
-	private Button editButton,deleteButton;
-	
+	private Button editButton, deleteButton;
+	private String whereClause;
+	private String[] whereArgs;
 
-  
-	
+	public String getWhereClause() {
+		return whereClause;
+	}
+
+	public void setWhereClause(String whereClause) {
+		this.whereClause = whereClause;
+	}
+
+	public String[] getWhereArgs() {
+		return whereArgs;
+	}
+
+	public void setWhereArgs(String[] whereArgs) {
+		this.whereArgs = whereArgs;
+	}
+
 	private LayoutInflater li;
-	
+
 	public EditPasswordDialog(LayoutInflater li) {
 		super();
 		this.li = li;
@@ -38,88 +54,92 @@ public class EditPasswordDialog extends DialogFragment {
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		
-		
-		
-  	View v =li.inflate(R.layout.activity_add_password, null);	   ; 	
-   	 
-   	 initDialog(v);
-   	setDialogData();
-   	
-   	final Dialog dlg = new AlertDialog.Builder(getActivity()).setView(v)
-			
-			.create();
-   	
-   	editButton.setOnClickListener(new View.OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			setDialogData();
-			Log.e("onclick", "doInBackground");
-			dataSource.setContext(getActivity().getApplicationContext());
-			dbHelper.update(null, null);
-			dlg.dismiss();
-			
-		}
-	});
-   	deleteButton.setOnClickListener(new View.OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			setDialogData();
-			Log.e("onclick", "doInBackground");
-			dataSource.setContext(getActivity().getApplicationContext());
-			dbHelper.delete(null, null);
-			dlg.dismiss();
-			
-		}
-	});
-		
+
+		View v = li.inflate(R.layout.activity_add_password, null);
+		;
+
+		initDialog(v);
+		setDialogData();
+
+		final Dialog dlg = new AlertDialog.Builder(getActivity()).setView(v)
+
+		.create();
+
+		editButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setDialogData();
+				Log.e("onclick", "doInBackground");
+
+				dbHelper.update(whereClause, whereArgs);
+				dlg.dismiss();
+
+			}
+		});
+		deleteButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				setDialogData();
+				Log.e("onclick", "doInBackground");
+				dbHelper.delete(whereClause, whereArgs);
+				dlg.dismiss();
+
+			}
+		});
+
 		return dlg;
 	}
 
-	
 	private void initDialog(View v) {
 		// TODO Auto-generated method stub
-		accountName =(EditText) v.findViewById(R.id.accountName);
-		username =(EditText)v.findViewById(R.id.username);
-		catagory =(Spinner)v.findViewById(R.id.catagorySpinner);
-		password = (EditText)v.findViewById(R.id.password); 
-		editButton =(Button)v.findViewById(R.id.editButton);
-		deleteButton =(Button)v.findViewById(R.id.deleteButton);
-		
+		accountName = (EditText) v.findViewById(R.id.accountName);
+		username = (EditText) v.findViewById(R.id.username);
+		catagory = (Spinner) v.findViewById(R.id.catagorySpinner);
+		password = (EditText) v.findViewById(R.id.password);
+		editButton = (Button) v.findViewById(R.id.editButton);
+		deleteButton = (Button) v.findViewById(R.id.deleteButton);
+		dataSource.setContext(getActivity().getApplicationContext());
+		String args[] = new String[4];
+		args[0] = dataSource.getCatagory();
+		args[1] = dataSource.getAccountName();
+		args[2] = dataSource.getUsername();
+		args[3] = dataSource.getPassword();
+
+		setWhereClause("catagory = ? and accountName = ? "
+				+ "and username = ? and password = ? ");
+		setWhereArgs(args);
+
 		accountName.setText(dataSource.getAccountName());
 		username.setText(dataSource.getUsername());
 		password.setText(dataSource.getPassword());
-		int i=0;
-		for(i=0;i<Constants.CATAGORY_SPINNER_ARRAY.length;i++)
-		{
-			if(dataSource.getCatagory().equalsIgnoreCase(Constants.CATAGORY_SPINNER_ARRAY[i]))
+		int i = 0;
+		for (i = 0; i < Constants.CATAGORY_SPINNER_ARRAY.length; i++) {
+			if (dataSource.getCatagory().equalsIgnoreCase(
+					Constants.CATAGORY_SPINNER_ARRAY[i]))
 				break;
 		}
 		catagory.setSelection(i);
-		
-		  @SuppressWarnings({ "rawtypes", "unchecked" })
-		ArrayAdapter adapter = new ArrayAdapter(getActivity().getApplicationContext(),
-		            R.layout.spinner_item, Constants.CATAGORY_SPINNER_ARRAY);
-		  adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-			catagory.setAdapter(adapter);
-		
-		  
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		ArrayAdapter adapter = new ArrayAdapter(getActivity()
+				.getApplicationContext(), R.layout.spinner_item,
+				Constants.CATAGORY_SPINNER_ARRAY);
+		adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+		catagory.setAdapter(adapter);
+
 	}
-	
-	
-	 private void setDialogData() {
-			// TODO Auto-generated method stub
-	    	dataSource.setAccountName(accountName.getText().toString());
-	    	dataSource.setUsername(username.getText().toString());
-	    	dataSource.setCatagory(catagory.getSelectedItem().toString());
-	    	dataSource.setPassword(password.getText().toString());
-			
-		}
-	
-		
-	
+
+	private void setDialogData() {
+		// TODO Auto-generated method stub
+		dataSource.setAccountName(accountName.getText().toString());
+		dataSource.setUsername(username.getText().toString());
+		dataSource.setCatagory(catagory.getSelectedItem().toString());
+		dataSource.setPassword(password.getText().toString());
+
+	}
+
 }
