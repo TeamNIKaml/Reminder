9bc36a933ecf29fb50a99e3cbb7e17c70c29db8a
Nikhil Vijayakumar <nikhil.vijaykumar@gmail.com>
2015-01-26 18:51:24 +0530

    eod 6:51 26-1-2015

--- ReminderTest/src/com/teamNIKaml/reminder/test/functionalTest/LoginActivityFunctionalTest.java
+++ ReminderTest/src/com/teamNIKaml/reminder/test/functionalTest/LoginActivityFunctionalTest.java
@@ -1,24 +1,22 @@
 package com.teamNIKaml.reminder.test.functionalTest;
 
-import android.content.Intent;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.ActivityUnitTestCase;
+import android.test.TouchUtils;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.teamNIKaml.reminder.activity.LoginActivity;
+import com.teamNIKaml.reminder.activity.PostLoginActivity;
 import com.teamNIKaml.reminder.activity.R;
 
 public class LoginActivityFunctionalTest extends
-	ActivityUnitTestCase<LoginActivity> {
+	ActivityInstrumentationTestCase2<LoginActivity> {
 
-    private LoginActivity loginActivity;
-    private TextView appName;
-    private EditText passWord;
-    private Button loginButton;
-    private Intent loginIntent;
+    private static final int TIMEOUT_IN_MS = 5000;
+    private static final String TEST_MESSAGE = "Hello Receiver";
+    private LoginActivity mSenderActivity;
 
     public LoginActivityFunctionalTest() {
 	super(LoginActivity.class);
@@ -27,36 +25,64 @@ public class LoginActivityFunctionalTest extends
     @Override
     protected void setUp() throws Exception {
 	super.setUp();
-
-	loginIntent = new Intent(getInstrumentation().getTargetContext(),
-		LoginActivity.class);
-
-	loginActivity = getActivity();
-	appName = (TextView) loginActivity.findViewById(R.id.app_name);
-	passWord = (EditText) loginActivity.findViewById(R.id.password);
-	loginButton = (Button) loginActivity.findViewById(R.id.loginButton);
+	setActivityInitialTouchMode(true);
+	mSenderActivity = getActivity();
     }
 
+    /**
+     * Tests the preconditions of this test fixture.
+     */
+    @MediumTest
     public void testPreconditions() {
-
-	assertNotNull("loginActivity is null", loginActivity);
-	assertNotNull("appName is null", appName);
-	assertNotNull("passWord is null", passWord);
-	assertNotNull("Login Button is null", loginButton);
+	assertNotNull("mSenderActivity is null", mSenderActivity);
     }
 
     @MediumTest
-    public void testNextActivityWasLaunchedWithIntent() {
+    public void testSendMessageToReceiverActivity() {
 
-	startActivity(loginIntent, null, null);
-	loginButton.performClick();
+	// Because this functional test tests interaction across multiple
+	// components these views
+	// are part of the actual test method and not of the test fixture
+	final Button loginButton = (Button) mSenderActivity
+		.findViewById(R.id.loginButton);
+	final EditText passwordEditText = (EditText) mSenderActivity
+		.findViewById(R.id.password);
 
-	final Intent launchIntent = getStartedActivityIntent();
+	// Create and add an ActivityMonitor to monitor interaction between the
+	// system and the
+	// ReceiverActivity
+	Instrumentation.ActivityMonitor receiverActivityMonitor = getInstrumentation()
+		.addMonitor(PostLoginActivity.class.getName(), null, false);
 
-	assertNotNull("Intent was null", launchIntent);
+	// Request focus on the EditText field. This must be done on the
+	// UiThread because
+	getInstrumentation().runOnMainSync(new Runnable() {
+	    @Override
+	    public void run() {
+		passwordEditText.requestFocus();
+	    }
+	});
+	// Wait until all events from the MainHandler's queue are processed
+	getInstrumentation().waitForIdleSync();
 
-	assertTrue(isFinishCalled());
+	// Send the text message
+	getInstrumentation().sendStringSync(TEST_MESSAGE);
+	getInstrumentation().waitForIdleSync();
 
+	// Click on the loginButton to send the message to ReceiverActivity
+	TouchUtils.clickView(this, loginButton);
+
+	// Wait until ReceiverActivity was launched and get a reference to it.
+	PostLoginActivity receiverActivity = (PostLoginActivity) receiverActivityMonitor
+		.waitForActivityWithTimeout(TIMEOUT_IN_MS);
+	// Verify that ReceiverActivity was started
+	assertNotNull("ReceiverActivity is null", receiverActivity);
+	assertEquals("Monitor for ReceiverActivity has not been called", 1,
+		receiverActivityMonitor.getHits());
+	assertEquals("Activity is of wrong type", PostLoginActivity.class,
+		receiverActivity.getClass());
+
+	// Unregister monitor for ReceiverActivity
+	getInstrumentation().removeMonitor(receiverActivityMonitor);
     }
-
 }
