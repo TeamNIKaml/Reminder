fdf1706916266395675f18726ee93f1cc732a6e2
Nikhil Vijayakumar <nikhil.vijaykumar@gmail.com>
2015-01-26 15:17:05 +0530

    LoginActivity unittest 3:16pm 26-1-2015

--- ReminderTest/src/com/teamNIKaml/reminder/test/unitTest/uiComponents/LoginActivityTest.java
+++ ReminderTest/src/com/teamNIKaml/reminder/test/unitTest/uiComponents/LoginActivityTest.java
@@ -1,6 +1,8 @@
 package com.teamNIKaml.reminder.test.unitTest.uiComponents;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.teamNIKaml.reminder.activity.LoginActivity;
@@ -9,13 +11,10 @@ import com.teamNIKaml.reminder.activity.R;
 public class LoginActivityTest extends
 	ActivityInstrumentationTestCase2<LoginActivity> {
 
-    public LoginActivityTest(Class<LoginActivity> activityClass) {
-	super(activityClass);
-	// TODO Auto-generated constructor stub
-    }
-
     private LoginActivity loginActivity;
     private TextView appName;
+    private EditText passWord;
+    private Button loginButton;
 
     public LoginActivityTest() {
 	super(LoginActivity.class);
@@ -27,19 +26,36 @@ public class LoginActivityTest extends
 
 	loginActivity = getActivity();
 	appName = (TextView) loginActivity.findViewById(R.id.app_name);
+	passWord = (EditText) loginActivity.findViewById(R.id.password);
+	loginButton = (Button) loginActivity.findViewById(R.id.loginButton);
     }
 
     public void testPreconditions() {
 
 	assertNotNull("loginActivity is null", loginActivity);
 	assertNotNull("appName is null", appName);
+	assertNotNull("passWord is null", passWord);
     }
 
-  
     public void testAppNameTextView() {
 
 	final String expected = loginActivity.getString(R.string.app_name);
 	final String actual = appName.getText().toString();
 	assertEquals("appName contains wrong text", expected, actual);
     }
+
+    public void testPasswordEditText() {
+
+	final String expected = "Enter your new password";
+	final String actual = passWord.getHint().toString();
+	assertEquals("Password Hint contains wrong text", expected, actual);
+    }
+
+    public void testLoginButton() {
+
+	final String expected = "Register";
+	final String actual = loginButton.getText().toString();
+	assertEquals("Password Hint contains wrong text", expected, actual);
+    }
+
 }
