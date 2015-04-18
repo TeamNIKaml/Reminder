90646a048acb0404e23935b8e3e382326b76dc0a
Nikhil <nikhil.vijaykumar@gmail.com>
2015-04-18 11:57:03 +0530

    About us

---
+++ Reminder/src/com/teamNIKaml/reminder/activity/AboutUs.java
@@ -0,0 +1,15 @@
+package com.teamNIKaml.reminder.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutUs extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_about_us);
+
+	}
+
+}
